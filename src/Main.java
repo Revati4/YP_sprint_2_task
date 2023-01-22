ec2a1fd3456711be1dbc65d760d24a7ef087a87b
Revati4 <o.a.bat@yandex.ru>
2023-01-22 11:55:55 +0300

    variant 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scn = new Scanner(System.in);
+        int guests;
+        while (true) {
+            System.out.print("Введите количество человек, на которых нужно разделить сумму чека: ");
+            guests = scn.nextInt();
+            if (guests > 1) {
+                System.out.println("Сумма чека будет разделена на "+ guests + " человек");
+                // наверное, надо отсюда еще  класс Calculate запускать, если так можно
+                break;
+            } else if (guests < 1) {
+                System.out.println("Количество человек введено не верно.");
+            } else if (guests == 1) {
+                System.out.println("Один человек оплатит чек в полной сумме.");
+            }
+        }
+
     }
 }
